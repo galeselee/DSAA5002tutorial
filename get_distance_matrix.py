3f9e606f4c2031cc920ac3c69b5dbf0abed2460a
Zeyu Li <zeyuli0123@icloud.com>
2024-04-01 14:29:02 +0800

    update get distrance matrix

--- get_distance_matrix.py
+++ get_distance_matrix.py
@@ -1,6 +1,16 @@
 import numpy as np
 import copy
 
+import argparse
+CLI=argparse.ArgumentParser()
+CLI.add_argument(
+    "--array",
+    nargs="*",
+    type=int,
+    default=[1, 2, 3],
+)
+
+args = CLI.parse_args()
 
 def get_distance(a, b):
     if not isinstance(a, list):
@@ -11,9 +21,21 @@ def get_distance(a, b):
             dis += (a[inner_ii]-b[inner_ii])**2
         dis = np.sqrt(dis)
         return dis
-dis = []
 
-array = [
+array = args.array
 
-]
+dis = np.zeros((len(array), len(array)))
+for ii in range(len(array)):
+    for jj in range(ii, len(array)):
+        dis[ii][jj] = get_distance(array[ii], array[jj])
+
+for ii in range(len(array)):
+    for jj in range(0, ii):
+        dis[ii][jj] = dis[jj][ii]
+
+print(dis)
+
+
+
+
 
